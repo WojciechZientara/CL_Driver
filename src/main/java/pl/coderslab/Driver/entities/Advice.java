0d12e2092a2e7298d1ff267be6aeb880488f89a5
Wojciech Zientara <wuze88@gmail.com>
2020-01-18 18:21:21 +0100

    Add Advice entity

---
+++ src/main/java/pl/coderslab/Driver/entities/Advice.java
@@ -0,0 +1,47 @@
+package pl.coderslab.Driver.entities;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.aspectj.weaver.ast.Test;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Getter @Setter @NoArgsConstructor
+@Table(name = "advices")
+public class Advice {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @NotNull
+    private User user;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @Lob
+    @Column(columnDefinition = "BLOB")
+    private byte[] appendix;
+
+    @OneToOne
+    private Test test;
+
+    @OneToMany
+    private Set<Display> displays;
+
+    @OneToMany
+    private Set<Conversation> conversation;
+
+    private LocalDateTime created;
+}
